3dc4d9f43776f8677d1e1079b528924562645f91
Yonas21 <yonalem21@gmail.com>
2023-06-28 07:00:02 +0300

    express generator bioleplate and database config added

---
+++ server/entity/book.schema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const bookSchema = mongoose.Schema({
+  title: {
+  type: String,
+  required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  discount: {
+    type: Number,
+    required: true
+  },
+  coverImage: {
+    type: String,
+    required: true
+  },
+  price:{
+    type: Number,
+    required: true
+  }
+})
+// Export model
+module.exports = mongoose.model('Book', 
+  bookSchema)
